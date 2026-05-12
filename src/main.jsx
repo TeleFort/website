6b8164ad7ecc3e69e553aa6e140910c8cf42b047
TeleFort <telefort@gmail.com>
2026-05-12 05:22:01 +0500

    Fix GitHub Pages SPA redirect

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,9 @@
 
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, null, redirect);
+}
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App.jsx';
